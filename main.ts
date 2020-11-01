42f572faa2c18fa0e07b2a8302f43518efb48a3a
TurquoiseTNT <69688534+TurquoiseTNT@users.noreply.github.com>
2020-11-01 18:36:20 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,62 @@
+devices.onNotified(MesDeviceInfo.IncomingCall, function () {
+    basic.showLeds(`
+        . # # # #
+        . # # . #
+        . . . . #
+        . # # . #
+        . # # # #
+        `)
+    music.playMelody("C5 A C5 B C5 C B A ", 125)
+    basic.showLeds(`
+        . # # # #
+        . # # . #
+        . . . . #
+        . # # . #
+        . # # # #
+        `)
+    basic.clearScreen()
+})
+bluetooth.onBluetoothConnected(function () {
+    basic.showLeds(`
+        . . # # .
+        # . # . #
+        . # # # .
+        # . # . #
+        . . # # .
+        `)
+    basic.clearScreen()
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # # # #
+        . # . . #
+        . # . . #
+        # # . # #
+        # # . # #
+        `)
+    music.playMelody("B C5 A B G A F E ", 125)
+    music.playMelody("B C5 A B G A F E ", 125)
+    music.playMelody("B C5 A B G A F E ", 125)
+    music.playMelody("B C5 A B G A F E ", 125)
+    music.playMelody("B C5 A B G A F E ", 125)
+    music.playMelody("B C5 A B G A F E ", 125)
+    music.playMelody("B C5 A B G A F E ", 125)
+    basic.showLeds(`
+        . # # # #
+        . # . . #
+        . # . . #
+        # # . # #
+        # # . # #
+        `)
+    basic.clearScreen()
+})
